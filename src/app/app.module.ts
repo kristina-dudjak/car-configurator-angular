9460023a60940d1b9ba7973a1e04b1eaaa407a9f
Kristina Dudjak <dudjakkristina@gmail.com>
2022-10-10 14:02:44 +0200

    Add theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { AngularFireModule } from '@angular/fire/compat'
+import { BrowserModule } from '@angular/platform-browser'
+import { environment } from 'src/environments/environment'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { RegisterComponent } from './views/register/register.component'
+import { LoginComponent } from './views/login/login.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'
+import { AuthRoutingModule } from './auth-routing.module'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatButtonModule } from '@angular/material/button'
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AngularFireAuthModule,
+    AuthRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    MatButtonModule
+  ]
+})
+export class AuthModule {}
